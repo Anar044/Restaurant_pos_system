dc7f021db0136e21a7bf24d48f45bb8e138b0947
Anar044 <anarg044@gmail.com>
2026-09-17 22:05:27 +0400

    Add POS printer configuration state

---
+++ services/restaurant-node/src/RestaurantNode.Api/Infrastructure/Migrations/20260917180331_AddPosPrinterConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantNode.Api.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPosPrinterConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfigured",
+                table: "printers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDefault",
+                table: "printers",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfigured",
+                table: "printers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDefault",
+                table: "printers");
+        }
+    }
+}
