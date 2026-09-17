00b3abd5fb074af25b7d07c71680879e4755dd98
Anar044 <51967882+Anar044@users.noreply.github.com>
2026-09-17 16:32:08 +0400

    Register BackOffice permission policies and tenant context

--- services/restaurant-node/src/RestaurantNode.Api/Program.cs
+++ services/restaurant-node/src/RestaurantNode.Api/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using RestaurantNode.Api.Features.Auth;
+using RestaurantNode.Api.Features.BackOffice;
 using RestaurantNode.Api.Features.Halls;
 using RestaurantNode.Api.Features.Menu;
 using RestaurantNode.Api.Features.Orders;
@@ -43,9 +44,8 @@ builder.Services
 
 builder.Services.AddAuthorization(options =>
 {
-    options.AddPolicy("menu.read", policy => policy.RequireClaim("permission", "menu.read"));
-    options.AddPolicy("orders.read", policy => policy.RequireClaim("permission", "orders.read"));
-    options.AddPolicy("orders.write", policy => policy.RequireClaim("permission", "orders.write"));
+    foreach (var permission in Permissions.All)
+        options.AddPolicy(permission, policy => policy.RequireClaim("permission", permission));
 });
 builder.Services.AddCors(options =>
 {
@@ -77,6 +77,7 @@ app.UseAuthorization();
 app.MapGet("/health", () => Results.Ok(new { status = "ok", service = "restaurant-node", utc = DateTimeOffset.UtcNow }));
 
 app.MapAuthEndpoints();
+app.MapBackOfficeEndpoints();
 app.MapHallEndpoints();
 app.MapMenuEndpoints();
 app.MapOrderEndpoints();
